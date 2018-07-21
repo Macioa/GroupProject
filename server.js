cafd988d37b2027340902138b5d5ccff053408c4
Ryan Montgomery <ryan.maciomaire@gmail.com>
2018-07-20 20:14:48 -0500

    express server

--- server.js
+++ server.js
@@ -1,2 +1,12 @@
+const port = 4000
+//npm install express nodemon chalk method-override body-parser mongodb mongoose
+
+const chalk = require('chalk');
+
 const express = require('express');
-const app = express()
+const app = express();
+
+
+app.listen(port, ()=>{
+    console.log(chalk.green("What's up? ")+chalk.grey(`Listening on port ${port}`))
+});
