f9d7edfdef556d16ab870fd620c1f0deabad241e
Daniel Negassi <dyn29@hotmail.com>
2018-07-23 12:19:51 -0500

    eventcontroller fixed

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -1,46 +1,94 @@
 const express = require('express');
-router = express.Router();
+const router = express.Router();
 const chalk = require('chalk');
+const Event = require('../Models/Event');
+const Location = require('../Models/Location');
+const User = require('../Models/user');
 
-//console.log(chalk.green('eventController connected'));
 
-router.get('/new', (req, res)=>{
-    res.render('./events/new.ejs',{
-//        user:user
-    })
+// INDEX
+router.get('/', async (req, res, err) => {
+  try {
+    console.log('hits index route')
+    const foundEvents = await Event.find();
+    res.render('Users/Home.ejs', {
+      event: foundEvents,
+      user: user
+    });
+
+  } catch (err) {
+    res.send(err, ' error for index route')
+  }
+});
+
+////New
+router.get('/new', (req, res) => {
+  res.render('events/New.ejs')
 })
 
-router.post('/', (req, res)=>{
-    // create new event
-})
+/// SHOW
+router.get('/:id', async (req, res, next) => {
+  try {
+    console.log('hits the show page');
+    const foundEvent = await Event.findById(req.params.id);
+    res.render('Events/Show.ejs', {
+      event: foundEvent,
+      user: user
+    });
+  } catch (err) {
+    res.send(err, ' error for show route');
+  }
+});
 
-router.get('/', (req, res)=>{
-    console.log(req.originalUrl)
-    res.render('./home.ejs',{
-//        eventList: events
-    })
-})
+///// EDIT
+router.get('/:id/edit', async (req, res) => {
+  try {
+    console.log('hits the edit page')
+    const foundEvent = await Event.findById(req.params.id);
+    res.render('Events/edit.ejs', {
+      event: foundEvent,
+    });
+  } catch (err) {
+    res.send(err, ' error for edit route');
+  }
+});
 
-router.get('/:id', (req, res)=>{
-    res.render('./events/show.ejs', {
-//        user:user,
-//        event:event
-    })
-})
 
-router.get('/:id/edit', (req, res)=>{
-    res.render('./events/edit.ejs', {
-//       event: event,
-//        user: user
-    })
-})
+/////// PUT = UPDATE
+router.put('/:id', async (req, res) => {
+  try {
+    console.log('hits the update page');
+    const updatedEvent = await Event.findByIdAndUpdate(req.params.id, req.body, {
+      new: true
+    });
+    res.redirect('/events')
+  } catch (err) {
+    res.send(err, ' error for update route');
+  }
+});
 
-router.put('/:id/edit', (req, res)=>{
+//////////// CREATE
+router.post('/', async (req, res) => {
+  try {
+    console.log(' hits the post route');
+    const newEvent = await Event.create(req.body);
 
-})
+    res.redirect('/events');
+  } catch (err) {
+    res.send(err, ' not creating a post');
+  }
+});
 
-router.delete('/:id', (req, res)=>{
+// DELETE AN AUTHOR DELETE THE ASSOCIATED ARTICLES
+router.delete('/:id', async (req, res) => {
+  try {
+    console.log('hits the delete in index')
+    const deletedEvent = await Event.findByIdAndRemove(req.params.id);
+  }
+  res.redirect('/events')
 
-})
-
-module.exports=router;
+} catch (err) {
+  console.log(err, ' not deleting');
+  res.send(err, ' not deleting');
+}
+});
